7b7425579b8985cf5fb84253e20388cbe1ce9d55
FairTee <stephenodjidja7@gmail.com>
2024-02-01 03:44:46 -0800

    Setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+USER_GROUP='root:root'
+# Install Nginx if not already installed
+if [ ! -x "$(command -v nginx)" ]; then
+    apt-get update
+    apt-get -y install nginx
+fi
+
+# Create necessary directories
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create fake HTML file
+echo -e "<html>\n<head>\n</head>\n<body>\n  Holberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
+
+# Create or recreate symbolic link
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test /data/web_static/current
+
+# Give ownership to ubuntu user and group recursively
+chown -R $USER_GROUP /data
+
+# Update Nginx configuration
+config_text="location /hbnb_static {\n\talias /data/web_static/current/;\n\tindex index.html;\n}\n"
+sed -i "/server_name _;/a $config_text" /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
+exit 0
